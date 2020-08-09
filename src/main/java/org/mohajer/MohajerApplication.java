c5893283846ce8e90a4cdba8deb08bf6a3c4dcf2
hojatv <hojat@Hojatollahs-MBP.fritz.box>
2020-08-09 08:28:54 +0200

    besmellah

---
+++ src/main/java/org/mohajer/MohajerApplication.java
@@ -0,0 +1,13 @@
+package org.mohajer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MohajerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MohajerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/mohajer/MohajerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mohajer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MohajerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
