94558379d247af432f93885e39bf9d813b8e109b
hojat.vaheb <hojat.vaheb@auto1.com>
2020-08-21 16:52:58 +0200

    setting up the project

--- src/main/java/org/mohajer/MohajerApplication.java
+++ src/main/java/org/mohajer/MohajerApplication.java
@@ -1,10 +1,22 @@
 package org.mohajer;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @SpringBootApplication
 public class MohajerApplication {
+
+	@Bean
+	CommandLineRunner test(){
+		return args -> {
+		};
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MohajerApplication.class, args);
