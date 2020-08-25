636f9353fef074cbf6df508f88effb742d70d938
hojat.vaheb <hojat.vaheb@auto1.com>
2020-08-25 16:55:33 +0200

    Adding a failing test

---
+++ src/test/java/org/mohajer/controller/UserManagementControllerTest.java
@@ -0,0 +1,16 @@
+package org.mohajer.controller;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.junit.Assert.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class UserManagementControllerTest {
+
+    @Test
+    public void FailingTest(){
+        assertEquals(true,false);
+    }
+
+}
