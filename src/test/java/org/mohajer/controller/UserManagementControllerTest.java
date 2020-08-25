f17b8be77639ef1027138069ef6abb281fd90b80
hojat.vaheb <hojat.vaheb@auto1.com>
2020-08-25 16:55:33 +0200

    Adding a test

---
+++ src/test/java/org/mohajer/controller/UserManagementControllerTest.java
@@ -0,0 +1,17 @@
+package org.mohajer.controller;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@RunWith(MockitoJUnitRunner.class)
+public class UserManagementControllerTest {
+
+    @Test
+    public void test(){
+        assertEquals(true,true);
+    }
+
+}
